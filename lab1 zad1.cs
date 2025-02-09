0f3c727c1ed0fe78fd6caf21464a1f34d720bec8
ProgramowaniePyrzakDominikw69838 <166232819+ProgramowaniePyrzakDominikw69838@users.noreply.github.com>
2025-02-09 12:28:50 +0100

    Add files via upload

---
+++ lab1 zad1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Podaj b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Podaj c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        double delta = b * b - 4 * a * c;
+        Console.WriteLine($"Delta: {delta}");
+
+        if (delta > 0)
+        {
+            double x1 = (-b - Math.Sqrt(delta)) / (2 * a);
+            double x2 = (-b + Math.Sqrt(delta)) / (2 * a);
+            Console.WriteLine($"x1: {x1}, x2: {x2}");
+        }
+        else if (delta == 0)
+        {
+            double x = -b / (2 * a);
+            Console.WriteLine($"x: {x}");
+        }
+        else
+        {
+            Console.WriteLine("Brak pierwiastków rzeczywistych.");
+        }
+    }
+}
