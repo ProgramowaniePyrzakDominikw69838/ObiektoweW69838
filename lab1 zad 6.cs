55564dec2f4da50159a3f7c93ce015664c8de29f
ProgramowaniePyrzakDominikw69838 <166232819+ProgramowaniePyrzakDominikw69838@users.noreply.github.com>
2025-02-09 12:36:33 +0100

    Add files via upload

---
+++ lab1 zad 6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj ilość liczb:");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] tab = new int[n];
+
+        Console.WriteLine("Podaj liczby:");
+        for (int i = 0; i < n; i++)
+        {
+            tab[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (tab[j] > tab[j + 1])
+                {
+                    int temp = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("Posortowane liczby:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(tab[i] + " ");
+        }
+    }
+}
