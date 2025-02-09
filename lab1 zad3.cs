5f7b4475f39f6bc7dce1edfaf253314a2eb86d58
ProgramowaniePyrzakDominikw69838 <166232819+ProgramowaniePyrzakDominikw69838@users.noreply.github.com>
2025-02-09 12:30:26 +0100

    Add files via upload

---
+++ lab1 zad3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] liczby = new double[10];
+
+        Console.WriteLine("Podaj 10 liczb:");
+        for (int i = 0; i < 10; i++)
+        {
+            liczby[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        Console.WriteLine("Od pierwszego do ostatniego:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(liczby[i] + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Od ostatniego do pierwszego:");
+        for (int i = 9; i >= 0; i--)
+        {
+            Console.Write(liczby[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
