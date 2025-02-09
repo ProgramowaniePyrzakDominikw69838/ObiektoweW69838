b9104a2187e650b72045a691c57d6e9494ad0064
ProgramowaniePyrzakDominikw69838 <166232819+ProgramowaniePyrzakDominikw69838@users.noreply.github.com>
2025-02-09 12:32:13 +0100

    Add files via upload

---
+++ lab1 zad 4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] liczby = new double[10];
+        Console.WriteLine("Podaj 10 liczb:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            liczby[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        double suma = 0;
+        double iloczyn = 1;
+        double min = liczby[0];
+        double max = liczby[0];
+
+        for (int i = 0; i < 10; i++)
+        {
+            suma += liczby[i];
+            iloczyn *= liczby[i];
+            if (liczby[i] < min) min = liczby[i];
+            if (liczby[i] > max) max = liczby[i];
+        }
+
+        Console.WriteLine("Suma: " + suma);
+        Console.WriteLine("Iloczyn: " + iloczyn);
+        Console.WriteLine("Min: " + min);
+        Console.WriteLine("Max: " + max);
+    }
+}
